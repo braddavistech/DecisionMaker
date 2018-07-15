33d685e00da1728f67e9c4e5461f0a588c8458ad
Brad Davis <brad@braddavistech.com>
2018-07-15 14:49:01 -0500

    Added array into js file to allow for infinite options. User chooses when they are done and program gets random choice.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var diceRoll = Math.floor(Math.random()*2)+1;
+var answers=prompt('What is your 1st option? If done, please enter Done.'); //This is the first question to start.
+var numberArray=0; //This will keep track of the number of the item in the array.
+var itemNumber=1; //This will keep track of the quesiton number for the following prompt box.
+var myOptions = [answers.toUpperCase()]; //This will make all answers upper case to allow for condition checks.
 
-var choice1=prompt("What is option 1?");
-var choice2=prompt("What is option 2?");
+while (myOptions[numberArray] !== "DONE") {//This is the start of a while loop to continue to add options until user enters done.
+    itemNumber+=1;
+    var answers=prompt('What is option ' + itemNumber        + ' ? If done, please enter Done.');
+    myOptions.push (answers.toUpperCase());//Pushes answer into array.
+    numberArray+=1;
+    }
 
-document.write('<p id="boxTop">Looks like we are going to :<br><br>');
-
-if (diceRoll==1){
-  document.write('<p id="boxBottom">' + choice1);}
-else {
-  document.write('<p id="boxBottom">' + choice2);}
+var diceRoll = Math.floor(Math.random()* numberArray); //Creates random selection for array assignment.
+document.write('<p id="boxBottom">' + myOptions[diceRoll]);
